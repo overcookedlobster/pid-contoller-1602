eacefcfe858d03bd85be6159dfe1f14300bcd6e9
overcookedlobster <yahwistasalomo@gmail.com>
2022-12-06 02:34:29 +0700

    output display is now correct

--- main/FSM.h
+++ main/FSM.h
@@ -13,14 +13,18 @@
 #define SHIFT 2
 #define WAIT 3
 #define RAPID 4
-void fsm_deb_sel(int button, int *state, int *flag, int *state_pid){
+
+#define P_STRING "P Value:"
+#define I_STRING "I Value:"
+#define D_STRING "D Value:"
+void fsm_debouncing_sel(int button_sel, int *state, int *flag, int *state_pid){
 	switch(*state){
 		case STALL:
-			if(button) *state = COUNT;
+			if(button_sel) *state = COUNT;
 			break;	
 		case COUNT:
-			if(!button) *state = STALL;
-			else if(button && (*flag)) *state = SHIFT;
+			if(!button_sel) *state = STALL;
+			else if(button_sel && (*flag)) *state = SHIFT;
 			break;
 		case SHIFT:
 			*state_pid = (*state_pid+ 1)%3;
@@ -28,38 +32,41 @@ void fsm_deb_sel(int button, int *state, int *flag, int *state_pid){
 			*flag = 0;
 			break;
 		case WAIT:
-			if(!button) *state = STALL; 
+			if(!button_sel) *state = STALL; 
 			break;
 	}	
 
 }
 
-void fsm_deb_op(int button, int *state, int *flag, int *timer_flag, int *rapid_flag, float* value){
+void fsm_debouncing_op(int button_op, int *state, int *flag, int *timer_flag, int *rapid_flag, float* value){
 	switch(*state){
 		case STALL:
-			if(button) *state = COUNT;
+			if(button_op) *state = COUNT;
 			break;	
 		case COUNT:
-			if(!button) *state = STALL;
-			else if(button && (*flag)) *state = SHIFT;
+			if(!button_op) *state = STALL;
+			else if(button_op && (*flag)) *state = SHIFT;
 			break;
 		case SHIFT:
 			*state = WAIT;
 			*flag = 0;
-			*value += (button * 0.1);
+			if(*value > 0) *value += (button_op * 0.1);
+			else if (!(*value) && (button_op > 0)) *value += (button_op * 0.1); 
 			break;
 		case WAIT:
-			if(!button) *state = STALL; 
-			else if(timer_flag) {
+			if(!button_op) *state = STALL; 
+			else if(*timer_flag) {
 				*state = RAPID;
 				*timer_flag = 0;
 			}
 			break;
 		case RAPID:
-			if(!button) *state = STALL;
+			if(!button_op) *state = STALL;
 			else if(*rapid_flag){
-				*value += (button * 0.1);
+				if (*value > 0) *value += (button_op * 0.1);
+				else if (!(*value) && (button_op > 0)) *value += (button_op * 0.1); 
 				*rapid_flag = 0;
+				*state = WAIT;
 				}
 			break;
 
@@ -68,17 +75,17 @@ void fsm_deb_op(int button, int *state, int *flag, int *timer_flag, int *rapid_f
 }
 
 
-void fsm_set(int *state_set, char *p_string, char *i_string, char *d_string, char *out_string, float* out_val){
+void fsm_set(int *state_set, float *p_value, float *i_value, float *d_value, char *out_string, float* out_val){
 	switch(*state_set){
 		case STATE_P:
-			sprintf(out_string, "%s, %.2f", p_string, *out_val);
+			sprintf(out_string, "%s, %.2f", P_STRING, *p_value);
 		break;
 
 		case STATE_D:
-			sprintf(out_string, "%s, %.2f", d_string, *out_val);
+			sprintf(out_string, "%s, %.2f", D_STRING, *d_value);
 		break;
 		case STATE_I:
-			sprintf(out_string, "%s, %.2f", i_string, *out_val);
+			sprintf(out_string, "%s, %.2f", I_STRING, *i_value);
 		break;
 }
 }
--- main/main.c
+++ main/main.c
@@ -15,7 +15,7 @@
 #define LCD_COLS 16
 #define LCD_ROWS 2
 
-#define GPIO_INPUT_IO_0 5
+#define GPIO_INPUT_IO_0 23
 #define GPIO_INPUT_IO_1 18
 #define GPIO_INPUT_IO_2 19
 #define GPIO_INPUT_PIN_SEL ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1)| (1ULL<<GPIO_INPUT_IO_2))
@@ -28,10 +28,13 @@ esp_timer_handle_t timer_handle_1;
 esp_timer_create_args_t create_args_2;
 esp_timer_handle_t timer_handle_2;
 
-int button_sel;
+esp_timer_create_args_t create_args_3;
+esp_timer_handle_t timer_handle_3;
+int button_sel = 0;
 int button_op;
 int state_sel = 0;
 int state_op = 0;
+int state_pid = 0;
 int flag = 0;
 int timer_flag = 0;
 int rapid_flag = 0;
@@ -39,20 +42,18 @@ int rapid_flag = 0;
 float p_value = 0;
 float i_value = 0;
 float d_value = 0;
-float out_val = 0; 
+float *out_val; 
 
-char *p_string = "P Value:";
-char *i_string = "I Value:";
-char *d_string = "D Value:";
 char out_string[10];
 
 void LCD_DemoTask(void* param)
 {
-    char num[20];
     while (true) {
         LCD_home();
 //	sprintf(num, "%s", out_string);
         LCD_writeStr(out_string);
+	printf("button_op, %d, button_sel: %d\n", button_op, button_sel);
+	printf("state_op: %d, state_sel: %d, state_pid: %d\n", state_op, state_sel, state_pid);
         vTaskDelay(100 / portTICK_RATE_MS);
         }
   
@@ -66,18 +67,30 @@ void timer_expired_2(void *p){
 	rapid_flag = 1;
 
 }
+void timer_expired_3(void *p){
+	timer_flag = 1;
+
+}
 
 void timer_expired(void *p){
 	button_sel = gpio_get_level(GPIO_INPUT_IO_0) ? 0:1;
 	button_op = ((gpio_get_level(GPIO_INPUT_IO_1))? 0:(-1)) + ((gpio_get_level(GPIO_INPUT_IO_2))? 0:(1));
-	fsm_deb_sel(button_sel, &state_sel, &flag, &state_op);
-	fsm_deb_op(button_op, &state_op, &flag, &flag, &rapid_flag, &out_val);
-	fsm_set(&state_sel, p_string, i_string, d_string, out_string, &out_val);
+	fsm_debouncing_sel(button_sel, &state_sel, &flag, &state_pid);
+	fsm_set(&state_pid, &p_value, &i_value, &d_value, out_string, out_val);
+	if(state_pid == 0)
+	fsm_debouncing_op(button_op, &state_op, &flag, &timer_flag, &rapid_flag, &p_value);
+	else if(state_pid == 1)
+	fsm_debouncing_op(button_op, &state_op, &flag, &timer_flag, &rapid_flag, &d_value);
+	else if(state_pid == 2)
+	fsm_debouncing_op(button_op, &state_op, &flag, &timer_flag, &rapid_flag, &i_value);
 
 	if((state_sel == COUNT)||(state_op == COUNT)) esp_timer_start_once(timer_handle_1, 500000);
 	else esp_timer_stop(timer_handle_1);
 
-	if(state_sel == WAIT) esp_timer_start_periodic(timer_handle_2, 250000);
+	if((state_op == WAIT)) esp_timer_start_once(timer_handle_3, 500000);
+	else esp_timer_stop(timer_handle_3);
+
+	if(state_op == RAPID) esp_timer_start_once(timer_handle_2, 250000);
 	else esp_timer_stop(timer_handle_2);
 }
 
@@ -107,6 +120,11 @@ create_args_2.callback = timer_expired_2;
 create_args_2.dispatch_method = ESP_TIMER_TASK;
 create_args_2.name = "esp_timer_2";
 esp_timer_create(&create_args_2, &timer_handle_2);
+
+create_args_3.callback = timer_expired_3;
+create_args_3.dispatch_method = ESP_TIMER_TASK;
+create_args_3.name = "esp_timer_3";
+esp_timer_create(&create_args_3, &timer_handle_3);
 
 esp_timer_start_periodic(timer_handle , 10000);
 
